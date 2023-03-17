dd6ca0a966eb404ec5b88b21e026d11a701dd388
SERGIO ANDRES RAMIREZ GARCES <sergioramirez117075@correo.itm.edu.co>
2023-03-16 23:52:53 -0500

    Add project files.

---
+++ Bridge.Logic/MyBridge.cs
@@ -0,0 +1,63 @@
+﻿namespace Bridge.Logic
+{
+    public class MyBridge
+    {
+        private readonly string _text;
+
+
+        public MyBridge(string text)
+        {
+            _text = text;
+        }
+
+        public String IsValide()
+        {
+            int count = 0;
+
+            int n = _text[0];
+            if (n != '*' && _text[_text.Length - 1] != '*')
+            {
+                return "INVALIDO";
+            }
+
+            for (int i = 1; i < _text.Length - 1; i++)
+            {
+                if (!(_text[i].Equals('=') || _text[i].Equals('+')))
+                {
+                    return "INVALIDO";
+                }
+            }
+            for (int i = 1; i < _text.Length - 1; i++)
+            {
+                if (!(_text[i].Equals(_text[_text.Length - i - 1])))
+                {
+                    return "INVALIDO";
+                }
+            }
+            for (int i = 1; i < _text.Length - 1; i++)
+            {
+                if (_text[i].Equals('='))
+                {
+                    count++;
+
+                    if (count > 2)
+                    {
+                        if (i - 1 != (int)(_text.Length / 2))
+                        {
+                            return "INVALIDO";
+                        }
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            return "VALIDO";
+
+
+        }
+
+    }
+
+}
---
+++ Bridge.UI.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Bridge.Logic;
+
+Console.WriteLine("ingrese la estructura de su puente");
+string? theBridge = Console.ReadLine();
+var mybridge = new MyBridge($"{theBridge}");
+
+Console.WriteLine(mybridge.IsValide());
